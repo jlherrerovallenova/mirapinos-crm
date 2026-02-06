5a70ee3d025004d5ad9671891b669e102086f390
jlherrerovallenova <juanh73@gmail.com>
2026-02-06 22:55:14 +0100

    Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
@@ -7,9 +8,9 @@ export default {
         pine: {
           50: '#f0fdf4',
           100: '#dcfce7',
-          600: '#10b981', // Esmeralda Mirapinos
+          600: '#10b981', // Verde Esmeralda Mirapinos
           800: '#065f46',
-          900: '#064e3b', // Pino Profundo
+          900: '#064e3b', // Verde Pino Profundo
         },
       },
       borderRadius: {
