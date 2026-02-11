33a08b00d6f318cbb5548705f2a2cc992bfe029b
jlherrerovallenova <juanh73@gmail.com>
2026-02-11 22:46:58 +0100

    Updated supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,8 +2,8 @@
 import { createClient } from '@supabase/supabase-js';
 import { Database } from '../types/supabase';
 
-const supabaseUrl = import.meta.env.https://nbrbggvdtbvaoxsllemf.supabase.co;
-const supabaseAnonKey = import.meta.env.sb_publishable_Xu11tC3ME-aYxuMxVrOZdw_WVrgM2mD;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error('Faltan las variables de entorno de Supabase (VITE_SUPABASE_URL, VITE_SUPABASE_ANON_KEY)');
