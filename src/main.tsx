e25753135c7e4758e6cdbb3ed397655c84b80127
jlherrerovallenova <juanh73@gmail.com>
2026-02-13 23:58:26 +0100

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,17 @@
 // src/main.tsx
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom' // <--- FALTABA ESTO
 import './index.css'
-import App from './App.tsx'
-import { AuthProvider } from './context/AuthContext'
+import App from './App'
+import { AuthProvider } from './context/AuthContext' // <--- FALTABA ESTO
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
   </StrictMode>,
 )
