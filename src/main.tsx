7f3abed581191ebb2fe72331e054cde5d472f76b
jlherrerovallenova <juanh73@gmail.com>
2026-03-05 22:40:48 +0100

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 // src/main.tsx
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom' // <--- FALTABA ESTO
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App'
-import { AuthProvider } from './context/AuthContext' // <--- FALTABA ESTO
+import { AuthProvider } from './context/AuthContext'
 import { DialogProvider } from './context/DialogContext'
+
+// Importar el script de diagnóstico para exponerlo en la consola
+import './utils/connectionDiagnostic' 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
